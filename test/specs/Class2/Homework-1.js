70c60cf5d489d0310529fe6d66befc94986f05fb
Deepak Sharma <sharma.deepak8616@gmail.com>
2022-11-16 21:28:20 -0500

    Code from Class2

---
+++ test/specs/Class2/Homework-1.js
@@ -0,0 +1,18 @@
+/**
+ * Automate below testcase:
+ * 
+ * TC-1:
+ * Steps:
+ * 1. Launch facebook.com
+ * 2. Click Create New Account
+ * 3. Verify female-gender button is not selected
+ * 4. Verify male-gender button is not selected
+ * 5. Verify custom-gender button is not selected
+ * 6. If female gender is NOT selected, then click on female gender radio button
+ * 7. Verify female-gender button is selected
+ *
+ * 
+ * 
+ * 
+ * 
+ */
